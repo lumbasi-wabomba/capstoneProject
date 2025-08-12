3af1e84f9fb3447b621764607c4e7f1a7c8a9ffa
lumbasi-wabomba <lumbasiwk@gmail.com>
2025-08-12 21:30:42 +0300

    changes to the settings file

--- config/settings.py
+++ config/settings.py
@@ -87,7 +87,15 @@ DATABASES = {
         },
     }
 }
+import sys
 
+if 'test' in sys.argv:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': ':memory:',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
